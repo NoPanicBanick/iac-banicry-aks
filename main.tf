32bace627cc7f6e0a373ad612e5d0651abc4c00f
Ryley Banick <ryley.banick@icloud.com>
2023-02-10 18:17:59 +1300

    Add acr to group

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 locals {
   location  = "westus2"
-  base_name = "banicry-aks"
+  base_name = "banicry"
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = local.base_name
+  name     = "${local.base_name}-rg"
   location = local.location
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = local.base_name
+  name                = "${local.base_name}-aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "bani"
@@ -24,4 +24,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+# TODO - Move to another repo to avoid deleting images
+resource "azurerm_container_registry" "acr" {
+  name                = "${local.base_name}-acr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
