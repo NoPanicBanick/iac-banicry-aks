6652e6c80e9e05cbb3253bcc1642bff8266c25b6
Ryley Banick <ryley.banick@icloud.com>
2023-02-10 18:19:45 +1300

    Fix acr name

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # TODO - Move to another repo to avoid deleting images
 resource "azurerm_container_registry" "acr" {
-  name                = "${local.base_name}-acr"
+  name                = "${local.base_name}acr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
