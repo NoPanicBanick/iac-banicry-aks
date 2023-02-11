b1c59ad3f86ae5f2fa1b604317b48ff001e4b97c
Ryley Banick <ryley.banick@icloud.com>
2023-02-11 16:04:50 +1300

    Give AKS the ability to pull images

--- main.tf
+++ main.tf
@@ -35,3 +35,9 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled                 = false
   public_network_access_enabled = true
 }
+
+resource "azurerm_role_assignment" "aks_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
