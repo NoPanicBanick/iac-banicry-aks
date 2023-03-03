ed6fe518306169f0f4551c25ad785d606ff43b92
Ryley Banick <ryley.banick@icloud.com>
2023-03-03 17:33:05 +1300

    Add kubectl pipeline

--- iac/static_resources/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "ryleyterraformstate"
-    container_name       = "terraform"
-    key                  = "iac_banicry_static.tfstate"
-  }
-}
