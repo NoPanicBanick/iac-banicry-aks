84ba22ff27996e153385f003ee1bdcdcfc266ca1
Ryley Banick <ryley.banick@icloud.com>
2023-06-04 18:30:00 -0500

    remove azure back ups

--- iac/cluster/main.tf
+++ iac/cluster/main.tf
@@ -20,33 +20,33 @@ resource "digitalocean_kubernetes_cluster" "foo" {
 }
 
 # storage account for backups
-resource "azurerm_resource_group" "minecraft" {
-  name     = "minecraft-rg"
-  location = local.location_az
-}
+# resource "azurerm_resource_group" "minecraft" {
+#   name     = "minecraft-rg"
+#   location = local.location_az
+# }
 
-resource "azurerm_storage_account" "backups" {
-  name                     = "${local.base_name}backups"
-  resource_group_name      = azurerm_resource_group.minecraft.name
-  location                 = azurerm_resource_group.minecraft.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "backups" {
+#   name                     = "${local.base_name}backups"
+#   resource_group_name      = azurerm_resource_group.minecraft.name
+#   location                 = azurerm_resource_group.minecraft.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
 
-resource "azurerm_storage_share" "backups" {
-  name                 = "backups"
-  storage_account_name = azurerm_storage_account.backups.name
-  quota                = 50 # 50 gigabytes
-}
+# resource "azurerm_storage_share" "backups" {
+#   name                 = "backups"
+#   storage_account_name = azurerm_storage_account.backups.name
+#   quota                = 50 # 50 gigabytes
+# }
 
-resource "kubernetes_secret" "backup-acct-secret" {
-  metadata {
-    name = "backups-acct"
-  }
+# resource "kubernetes_secret" "backup-acct-secret" {
+#   metadata {
+#     name = "backups-acct"
+#   }
 
-  data = {
-    azurestorageaccountname = azurerm_storage_account.backups.name
-    azurestorageaccountkey  = azurerm_storage_account.backups.primary_access_key
-  }
-}
+#   data = {
+#     azurestorageaccountname = azurerm_storage_account.backups.name
+#     azurestorageaccountkey  = azurerm_storage_account.backups.primary_access_key
+#   }
+# }
 
